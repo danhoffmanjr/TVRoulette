cb471511de29af0bb9fc1e109d616ceee7f33f11
Dan Hoffman <dghoffman@gmail.com>
2018-01-10 20:07:59 -0500

    added finishing touches

--- script.js
+++ script.js
@@ -16,18 +16,24 @@
         collectionValue,
         minPage = 1,
         maxPage = 1,
-        page = '1',
-        selectionDetails = {};
+        page = '1';
 
     collection.addEventListener('change', function(){
       if(this.value == "select"){
         genresSelect.innerHTML = "";
+        collectionValue = "";
+        collection.style.color = "#aaa";
+        genresSelect.style.color = "#aaa";
       }else if(this.value == "movie"){
         queryGenres(method.get, 'genre/movie/list?api_key='+apiKey+'&language=en-US');
         collectionValue = 'movie';
+        collection.style.color = "red";
+        genresSelect.style.color = "red";
       }else{
         queryGenres(method.get, 'genre/tv/list?api_key='+apiKey+'&language=en-US');
         collectionValue = 'tv';
+        collection.style.color = "red";
+        genresSelect.style.color = "red";
       }
     });
 
@@ -36,6 +42,7 @@
         alert("Select 'Movie' or 'TV Show' from the menu to play.");
       }else {
         spinRoulette(method.get, 'discover/'+collectionValue+'?api_key='+apiKey+'&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres='+genresSelect.value);    
+        this.innerText = "Spin Again";
       }
     });
 
@@ -58,7 +65,7 @@
             newURI = baseURL + uri + '&page=' + page;
             getSelection(method, newURI);
           } else {
-            console.log('There was a problem with the request.');
+            console.log('[spinRoulette] There was a problem with the request.');
           }
         }
       };
@@ -84,7 +91,8 @@
             console.log("Random Selection:");
             console.log(selection);
           } else {
-            console.log('There was a problem with the request.');
+            console.log('[getSelection] There was a problem with the request.');
+            spinRoulette('GET', 'discover/'+collectionValue+'?api_key='+apiKey+'&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres='+genresSelect.value);
           }
         }
       };
@@ -108,7 +116,7 @@
               console.log(details);
             }
           } else {
-            console.log('There was a problem with the request.');
+            console.log('[getFullDetails] There was a problem with the request.');
           }
         }
       };
@@ -117,18 +125,36 @@
 
     function renderDetails(data){
       output.innerHTML = "";
-      if(data.title == undefined){
-        output.innerHTML += '<h3>' + data.name + '</h3>';
+      if(data.poster_path == null){
+        var imgPoster = "no-photo.jpg";
       }else{
-        output.innerHTML += '<h3>' + data.title + '</h3>';
+        var imgPoster = imageBaseURL + data.poster_path;
       }
-      output.innerHTML += '<p>' + data.overview + '</p>' +
-                          '<p><img src="' + imageBaseURL + data.poster_path + '"></p>' + 
-                          '<ul><li><a href="http://www.imdb.com/title/' + data.imdb_id + '/">IMDB</a></li>' +
-                          '<li>'+ data.homepage +'</li>' + 
-                          '<li>'+ data.release_date +'</li>' +
-                          '<li>'+ data.vote_average +'</li>' +
-                          '</ul>';
+      //display release date if available
+      if(data.release_date == undefined || data.release_date == null || data.release_date == ""){
+        var releaseDate = "";
+      }else{
+        var releaseDate = '<li>Release Date: '+ data.release_date +'</li>';
+      }
+      //display IMDB link if available
+      if(data.imdb_id == undefined || data.imdb_id == null || data.imdb_id == ""){
+        var imdbURL = "";
+      }else{
+        var imdbURL = '<li>IMDB Page: <a href="http://www.imdb.com/title/' + data.imdb_id + '/" target="_blank">http://www.imdb.com/title/' + data.imdb_id + '</a></li>';
+      }
+      //display website link if available
+      if(data.homepage == undefined || data.homepage == null || data.homepage == ""){
+        var websiteURL = "";
+      }else{
+        var websiteURL = '<li>Website: <a href="'+ data.homepage +'" target="_blank">'+ data.homepage +'</a></li>';
+      }
+      var extraDetails = '<p class="rating">Average Viewer Rating: <span>' + data.vote_average + '</span> ('+ data.vote_count +' reviews)</p><p>' + data.overview + '</p><ul>' + imdbURL + websiteURL + releaseDate + '</ul></div>';
+      output.innerHTML += '<div class="poster"><img src="' + imgPoster + '"></div>';
+      if(data.title == undefined){
+        output.innerHTML += '<div class="details"><h3>' + data.name + '</h3>' + extraDetails;
+      }else{
+        output.innerHTML += '<div class="details"><h3>' + data.title + '</h3>' + extraDetails;
+      }
     }
   
     function queryGenres(method, uri) {
@@ -144,7 +170,7 @@
             var genres = JSON.parse(httpRequest.responseText);
             createGenresSelect(genres.genres);
           } else {
-            console.log('There was a problem with the request.');
+            console.log('[queryGenres] There was a problem with the request.');
           }
         }
       };
@@ -157,14 +183,7 @@
       genresSelect.innerHTML = "";
       var option;
       genreArr.forEach(element => {
-        option = '<option value="' + element.id + '">' + element.name + '</option>';
-        genresSelect.innerHTML += option;
-      });
-    }
-
-    function displayTitles(data){
-      data.forEach((element, index) => {
-        console.log(index + " " + element.title);
+        genresSelect.innerHTML += '<option value="' + element.id + '">' + element.name + '</option>';
       });
     }
 
